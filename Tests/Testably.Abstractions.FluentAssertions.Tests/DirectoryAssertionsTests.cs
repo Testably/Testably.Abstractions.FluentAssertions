621f0ff68e031aa9bcd21e4b1a3e44417d837025
Valentin Breuß <v.breuss@tig.at>
2023-08-21 11:47:01 +0200

    Fix HasFilesMatching implementation for more than one file.

--- Tests/Testably.Abstractions.FluentAssertions.Tests/DirectoryAssertionsTests.cs
+++ Tests/Testably.Abstractions.FluentAssertions.Tests/DirectoryAssertionsTests.cs
@@ -66,4 +66,37 @@ public class DirectoryAssertionsTests
 			.Be(
 				$"Expected directory \"{directoryName}\" to contain at least one file matching \"{fileName}\" {because}, but none was found.");
 	}
+
+	[Theory]
+	[InlineAutoData(3, 5)]
+	[InlineAutoData(1, 2)]
+	public void HasFilesMatching_WithoutTooLittleFiles_ShouldThrow(
+		int matchingCount,
+		int expectedCount,
+		string directoryName,
+		string fileNamePrefix,
+		string because)
+	{
+		MockFileSystem fileSystem = new();
+		fileSystem.Initialize()
+			.WithSubdirectory(directoryName);
+		for (int i = 0; i < matchingCount; i++)
+		{
+			fileSystem.File.WriteAllText(
+				fileSystem.Path.Combine(directoryName, $"{fileNamePrefix}-{i}.txt"),
+				"some content");
+		}
+
+		DirectoryAssertions? sut = fileSystem.Should().HaveDirectory(directoryName).Which;
+
+		Exception? exception = Record.Exception(() =>
+		{
+			sut.HasFilesMatching($"{fileNamePrefix}*", expectedCount, because);
+		});
+
+		exception.Should().NotBeNull();
+		exception!.Message.Should()
+			.Be(
+				$"Expected directory \"{directoryName}\" to contain at least {expectedCount} files matching \"{fileNamePrefix}*\" {because}, but only {matchingCount} were found.");
+	}
 }
